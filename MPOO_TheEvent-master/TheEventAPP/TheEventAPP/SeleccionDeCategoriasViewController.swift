2fcfa3cd29b7cfad121b524b085b328d6be288a9
AdrianHyunam <30992237+AdrianHyunam@users.noreply.github.com>
2018-10-30 16:28:16 -0600

    Add files via upload

---
+++ MPOO_TheEvent-master/TheEventAPP/TheEventAPP/SeleccionDeCategoriasViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SeleccionDeCategoriasViewController.swift
+//  TheEventAPP
+//
+//  Created by Usuario invitado on 30/10/18.
+//  Copyright © 2018 Usuario invitado. All rights reserved.
+//
+
+import UIKit
+
+class SeleccionDeCategoriasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+}
