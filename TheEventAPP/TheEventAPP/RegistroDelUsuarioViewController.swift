3fab8e5077d6580be0d48e5958e7d8e863a258f3
AdrianHyunam <30992237+AdrianHyunam@users.noreply.github.com>
2018-11-14 15:37:12 -0600

    Add files via upload

---
+++ TheEventAPP/TheEventAPP/RegistroDelUsuarioViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegistroDelUsuarioViewController.swift
+//  TheEventAPP
+//
+//  Created by Usuario invitado on 30/10/18.
+//  Copyright © 2018 Usuario invitado. All rights reserved.
+//
+
+import UIKit
+
+class RegistroDelUsuarioViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+    
+
+}
